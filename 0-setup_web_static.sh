d5283025dbea00ae667ad4af51e2705bf2cbdaba
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-10-06 07:45:32 +0000

    Add 0 a

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,28 +15,11 @@ echo "<html>
 	</body>
 	</html>" | sudo tee  /data/web_static/releases/test/index.html
 
+
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo chmod -R 755 /data/
 
-sudo chown -R ubuntu:ubuntu /data/
-
-echo "server {
-	listen      80 default_server;
-	listen      [::]:80 default_server ipv6only=on;
-	server_name _;
-	location /redirect_me {
-		return 301 https://www.youtube.com/watch?v=DHITmcKUGik;
-	}
-	location /hbnb_static/ {
-		alias /data/web_static/current;
-	}
-	error_page 404 /404.html;
-	location /404 {
-		root /var/www/error/;
-		internal;
-	}
-	}" | sudo tee  /etc/nginx/sites-available/default
-	
+sudo sed -i '48 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default	
 
 sudo service nginx restart
-
-exit 0
