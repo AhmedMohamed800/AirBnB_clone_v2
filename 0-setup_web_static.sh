40b9326790bfcb9c73614f54315e72937958f182
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-10-06 06:33:06 +0000

    Add 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,10 +4,6 @@
 sudo apt-get update
 sudo apt-get install nginx -y
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test
 sudo touch /data/web_static/releases/test/index.html
 echo "<html>
