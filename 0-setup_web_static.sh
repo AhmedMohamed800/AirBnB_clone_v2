ee162d681442cd9d9be8ff3e4441e0a18132a3db
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-10-06 07:28:19 +0000

    Add 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,7 +4,8 @@
 sudo apt-get update
 sudo apt-get install nginx -y
 
-sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
 echo "<html>
 	<head>
@@ -18,6 +19,22 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i "/listen 80 default_server;/ a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+echo "server {
+	listen      80 default_server;
+	listen      [::]:80 default_server ipv6only=on;
+	server_name _;
+	location /redirect_me {
+		return 301 https://www.youtube.com/watch?v=DHITmcKUGik;
+	}
+	location /hbnb_static {
+		alias /data/web_static/current;
+	}
+	error_page 404 /404.html;
+	location /404 {
+		root /var/www/error/;
+		internal;
+	}
+	}" | sudo tee  /etc/nginx/sites-available/default
+	
 
 sudo service nginx restart
