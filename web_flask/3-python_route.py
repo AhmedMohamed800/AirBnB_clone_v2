a958916f10c00f20471ad458b4b9e085eb9fb814
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-10-20 18:00:23 +0000

    Add 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""starts a Flask web application:"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """print hello hbnb"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hhbnb():
+    """print hello hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def namec(text):
+    """print hello hbnb"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def namepython(text):
+    """print hello hbnb"""
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
