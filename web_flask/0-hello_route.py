9b7a89cd7d3304c85d71926e7dd8e28d4e7052b6
Ahmed Mohamed <ahmedmoh0107@gmail.com>
2023-10-20 17:41:43 +0000

    fix first task

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# starts a Flask web application:
+"""starts a Flask web application:"""
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_world():
+    """print hello hbnb"""
     return "Hello HBNB!"
 
 
